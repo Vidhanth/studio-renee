2c5ec60ad93e42af5e0ee1b05dad1aa5a3aa497e
Vidhanth <vidhanthbafna@gmail.com>
2022-07-01 13:21:08 +0530

    Update keywords

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -14,7 +14,7 @@ export const Head = ({ title = "", description = "Professional Architects and In
       <title>{title ? `${title} | ${defaultTitle}` : defaultTitle}</title>
       <meta
         name="keywords"
-        content="interiors, architecture, home, design, interior design, home designer, home interior, modern interior design, interior architect, design architects, bangalore, india, interior designer bangalore"
+        content="interiors, architecture, home, commercial, design, interior design, commercial designer, commercial interior, home designer, home interior, modern interior design, interior architect, design architects, bangalore, india, interior designer bangalore"
       />
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
