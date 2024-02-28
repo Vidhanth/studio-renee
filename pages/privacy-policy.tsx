27960506d6e74da994127ebed447c64bd8996869
Vidhanth <vidhanthbafna@gmail.com>
2024-02-29 01:19:08 +0530

    Update privacy policy

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -10,7 +10,7 @@ const PrivacyPolicy: NextPage = () => {
 
       <main>
         <div className="mx-auto mt-7 max-w-6xl px-7 sm:px-6 lg:mt-16 lg:px-8 text-justify">
-          <h1 className="text-3xl lg:text-[2.5rem] font-medium">
+          <h1 className="text-3xl lg:mb-4 lg:text-[2.5rem] font-medium">
             Privacy Policy
           </h1>
           <br />
@@ -30,10 +30,10 @@ const PrivacyPolicy: NextPage = () => {
           when you interact with our website, such as when you fill out contact
           forms or communicate with us via email. The types of personal
           information we may collect include your name, email address, phone
-          number, and any other information you choose to provide. Use of
-          Information We use the information we collect solely to respond to
-          your inquiries and provide you with the information or services you
-          request.
+          number, and any other information you choose to provide.
+          <div className="font-bold py-2">Use of Information</div>
+          We use the information we collect solely to respond to your inquiries
+          and provide you with the information or services you request.
           <div className="font-bold py-2">Disclosure of Information</div>
           We do not disclose your personal information to third parties unless
           required by law or to comply with legal obligations.
@@ -47,14 +47,19 @@ const PrivacyPolicy: NextPage = () => {
           <div className="font-bold py-2">Retention of Information</div>
           We will retain your personal information for as long as necessary to
           fulfil the purposes outlined in this Privacy Policy, unless a longer
-          retention period is required or permitted by law. Changes to this
-          Privacy Policy We reserve the right to update or modify this Privacy
-          Policy at any time. Any changes will be effective immediately upon
-          posting the revised policy on our website. We encourage you to review
-          this Privacy Policy periodically for any updates.
+          retention period is required or permitted by law.
+          <div className="font-bold py-2">Changes to this Privacy Policy</div>
+          We reserve the right to update or modify this Privacy Policy at any
+          time. Any changes will be effective immediately upon posting the
+          revised policy on our website. We encourage you to review this Privacy
+          Policy periodically for any updates.
           <div className="font-bold py-2">Contact Us</div>
           If you have any questions or concerns about this Privacy Policy or our
-          data practices, please contact us at studioreneedesigns@gmail.com
+          data practices, please contact us at{" "}
+          <a href="mailto:studioreneedesigns@gmail.com">
+            studioreneedesigns@gmail.com
+          </a>
+          .
         </div>
         <br />
         <br />
